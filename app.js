b92d39c019ddfc7a38c3e22a055388fa57295d60
Tatu Anttila <tatu.anttila@tuni.fi>
2023-08-22 12:40:45 +0300

    Fly.io deployment: HTML check and dummy end point added (exercise 11.11)

--- app.js
+++ app.js
@@ -3,14 +3,25 @@ const app = express()
 
 // Heroku dynamically sets a port
 // eslint-disable-next-line no-undef
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000 // for GitHub
 // eslint-disable-next-line no-undef
-// const PORT = process.env.PORT || 8080
+// const PORT = process.env.PORT || 8080 // for remote
 
 app.use(express.static('dist'))
 
-app.listen(PORT, () => {
-// app.listen(PORT, '0.0.0.0', () => {
+// For HTML health check (fly.io deployment)
+app.get('/health', (req, res) => {
+  // throw 'error...'
+  // eslint-disable-next-line no-unreachable
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('7') // change this string to ensure a new version deployed
+})
+
+app.listen(PORT, () => { // for GitHub
+// app.listen(PORT, '0.0.0.0', () => { // for remote
   // console.log('server started on port 5000')
   console.log(`server started on port ${PORT}`)
 })
