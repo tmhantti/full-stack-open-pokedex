ac8635eb14b1b118d0bf7a66959bcec157bfc785
Tatu Anttila <tatu.anttila@tuni.fi>
2023-08-22 07:48:13 +0300

    Additional test + changes to workflow (exercise 11.10)

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,8 +1,21 @@
 /* eslint-disable linebreak-style */
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    cy.visit('http://localhost:5000')
+    cy.visit('http://localhost:8080')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+})
+
+describe('Pokedex - Navigation Test', () => {
+  it('should navigate from HomePage to Ekans Pokemon Page', function() {
+    cy.visit('http://localhost:8080')
+    cy.get('a[href="/pokemon/ekans"]').click()
+
+    // confirm the new URL is the expected one
+    cy.url().should('include', '/pokemon/ekans')
+
+    // confirm the visited page contains the text 'ekans'
+    cy.contains('ekans').should('be.visible')
   })
 })
