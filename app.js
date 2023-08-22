531dab6ef0fc354ab616d3b4805a19ed688c5164
Tatu Anttila <tatu.anttila@tuni.fi>
2023-08-22 12:04:07 +0300

    Fly.io deployment step added (exercise 11.10)

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const app = express()
 // Heroku dynamically sets a port
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000
+// eslint-disable-next-line no-undef
+// const PORT = process.env.PORT || 8080
 
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log('server started on port 5000')
+// app.listen(PORT, '0.0.0.0', () => {
+  // console.log('server started on port 5000')
+  console.log(`server started on port ${PORT}`)
 })
