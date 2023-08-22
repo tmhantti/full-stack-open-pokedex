40f059232b1c0a632dde9e02ff9d97134c3f5b6c
Tatu Anttila <tatu.anttila@tuni.fi>
2023-08-22 14:02:44 +0300

    testing PR/broken deployment on another branch

--- app.js
+++ app.js
@@ -9,9 +9,11 @@ const PORT = process.env.PORT || 5000 // for GitHub
 
 app.use(express.static('dist'))
 
+// TESTING BROKEN DEPLOYMENT IN GITHUB ACTIONS
+
 // For HTML health check (fly.io deployment)
 app.get('/health', (req, res) => {
-  // throw 'error...'
+  throw 'error...'
   // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
